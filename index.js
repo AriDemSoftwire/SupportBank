a9dde753a6f16ab08b8d5a388faef07b24e17f06
AriDemSoftwire <arina.demidova@softwire.com>
2022-10-19 12:51:09 +0100

    Both faulty values are not registered in the logs

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 import log4js from "log4js";
-import fs from 'fs';
-import readlineSync from 'readline-Sync';
+import fs from "fs";
+import readlineSync from "readline-Sync";
+import moment from "moment";
 
-const logger = log4js.getLogger('debug');
+const logger = log4js.getLogger("debug");
 
 log4js.configure({
     appenders: {
-        file: { type: 'fileSync', filename: 'logs/debug.log' }
+        file: { type: "fileSync", filename: "logs/debug.log" },
     },
     categories: {
-        default: { appenders: ['file'], level: 'debug'}
-    }
+        default: { appenders: ["file"], level: "debug" },
+    },
 });
 
-logger.debug("log4js test");
+logger.debug("Programme starts");
 
-const lines = fs.readFileSync('Transactions2014.csv', 'utf-8').split('\n');
-let lines2 = fs.readFileSync('DodgyTransactions2015.csv', 'utf-8').split('\n');
-let lines3 = lines.concat(lines2);
+const lines = fs.readFileSync("Transactions2014.csv", "utf-8").split("\n");
+lines.pop();
+let lines2 = fs.readFileSync("DodgyTransactions2015.csv", "utf-8").split("\n");
+lines2.shift();
+const lines3 = lines.concat(lines2);
 
 class Transaction {
-    constructor (date, from, to, narrative, amount) {
+    constructor(date, from, to, narrative, amount) {
         this.date = date;
         this.from = from;
         this.to = to;
@@ -30,66 +33,80 @@ class Transaction {
 }
 
 class Account {
-
-    constructor (name, transactions) {
+    constructor(name, transactions) {
         this.name = name;
         this.transactionsTo = this.addTransactionsTo(transactions);
         this.transactionsFrom = this.addTransactionsFrom(transactions);
     }
 
-addTransactionsTo (transactions) {
-    const transactionsTo = transactions.filter(array => array.to === this.name)
-    return transactionsTo;
-}
-
-addTransactionsFrom (transactions) {
-    const transactionsFrom = transactions.filter(array => array.from === this.name)
-    return transactionsFrom;
-}
-
-calculateOwedSum () {
-    let totalFromSum = 0;
-    let totalToSum = 0;
-    let totalSum = 0;
-    for (let i = 0; i < this.transactionsFrom.length; i++) {
-        const addFrom = parseInt(this.transactionsFrom[i].amount);
-        totalFromSum += addFrom;
+    addTransactionsTo(transactions) {
+        const transactionsTo = transactions.filter(
+            (array) => array.to === this.name
+        );
+        return transactionsTo;
     }
-    for (let i = 0; i < this.transactionsTo.length; i++) {
-        const addTo = parseInt(this.transactionsTo[i].amount);
-        totalToSum += addTo;
-    }
-    totalSum = totalToSum - totalFromSum;
-    return totalSum;
-}
 
-listAllTransactions () {
-    console.log(this.name);
-    for (let i = 0; i < this.transactionsFrom.length; i++) {
-    console.log(this.transactionsFrom[i]);
+    addTransactionsFrom(transactions) {
+        const transactionsFrom = transactions.filter(
+            (array) => array.from === this.name
+        );
+        return transactionsFrom;
     }
-    for (let i = 0; i < this.transactionsTo.length; i++) {
-    console.log(this.transactionsTo[i]);
-}
 
-}
+    calculateOwedSum() {
+        let totalFromSum = 0;
+        let totalToSum = 0;
+        let totalSum = 0;
+        for (let i = 0; i < this.transactionsFrom.length; i++) {
+            const addFrom = parseFloat(this.transactionsFrom[i].amount);
+            totalFromSum += addFrom;
+        }
+        for (let i = 0; i < this.transactionsTo.length; i++) {
+            const addTo = parseFloat(this.transactionsTo[i].amount);
+            totalToSum += addTo;
+        }
+        totalSum = totalToSum - totalFromSum;
+        return Math.round(totalSum * 100) / 100;
+    }
+
+    listAllTransactions() {
+        console.log(this.name);
+        for (let i = 0; i < this.transactionsFrom.length; i++) {
+            console.log(this.transactionsFrom[i]);
+        }
+        for (let i = 0; i < this.transactionsTo.length; i++) {
+            console.log(this.transactionsTo[i]);
+        }
+    }
 }
 
 let arrayOfTransactions = [];
 let arrayOfNames = [];
 let arrayOfAccounts = [];
 
-function makeArrayOfTransactions (array) {
+function makeArrayOfTransactions(array) {
     let newArray = [];
-    for (let i = 1; i < array.length; i++) {
-        const line = array[i].split(',');
-        newArray[i] = new Transaction(line[0], line[1], line[2], line[3], line[4]);
+    for (let i = 1; i < array.length - 1; i++) {
+        const line = array[i].split(",");
+        newArray[i] = new Transaction(
+            line[0],
+            line[1],
+            line[2],
+            line[3],
+            line[4]
+        );
+        if (moment(line[0], "DD/MM/YYYY", true).isValid() === false) {
+           logger.debug(`Error: ${line[0]} is not a date. Array index is ${i}`);
+        }
+        if (parseFloat(line[4]) !== parseFloat(line[4])) {
+            logger.debug(`Error: ${line[4]} is not a number. Array index is ${i}`);
+        }
     }
     return newArray;
 }
 
-function makeArrayOfNames () {
-    for (let i = 1; i < arrayOfTransactions.length - 1; i++) {
+function makeArrayOfNames() {
+    for (let i = 1; i < arrayOfTransactions.length - 2; i++) {
         if (arrayOfNames.includes(arrayOfTransactions[i].to) === false) {
             arrayOfNames.push(arrayOfTransactions[i].to);
         }
@@ -97,7 +114,7 @@ function makeArrayOfNames () {
     return arrayOfNames;
 }
 
-function makeArrayOfAccounts () {
+function makeArrayOfAccounts() {
     let returnArray = [];
     for (let i = 0; i < arrayOfNames.length; i++) {
         returnArray[i] = new Account(arrayOfNames[i], arrayOfTransactions);
@@ -111,12 +128,18 @@ arrayOfNames = makeArrayOfNames();
 
 arrayOfAccounts = makeArrayOfAccounts();
 
-
-const userCommand = readlineSync.question('Type the command ');
-if (userCommand === 'List All') {
+const userCommand = readlineSync.question("Type the command ");
+if (userCommand === "List All") {
     for (let i = 0; i < arrayOfNames.length; i++) {
         const sum = arrayOfAccounts[i].calculateOwedSum();
-        console.log('Number: ', i, '     Name: ', arrayOfAccounts[i].name, '        Sum Owed: ', sum);
+        console.log(
+            "Number: ",
+            i,
+            "     Name: ",
+            arrayOfAccounts[i].name,
+            "        Sum Owed: ",
+            sum
+        );
     }
 }
 
@@ -124,4 +147,4 @@ for (let i = 0; i < arrayOfNames.length; i++) {
     if (userCommand === `List ${arrayOfNames[i]}`) {
         arrayOfAccounts[i].listAllTransactions();
     }
-}  
+}
